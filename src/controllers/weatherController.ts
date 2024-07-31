6bfbb5fccedad98a17a66ffbca8790a5f7702824
RichaLogiciel <richa@logiciel.io>
2024-07-31 18:03:31 +0530

    controller added

--- src/controllers/weatherController.ts
+++ src/controllers/weatherController.ts
@@ -6,12 +6,6 @@ const API_KEY = '65e92b00040ca46b3971462d1e350b13';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 export const getWeather = async(req: Request, res: Response) => {
-   try {
-        const response = await axios.get(`${BASE_URL}?q=${city}&appid=${API_KEY}&unit`)
-        return response.data;
-    } catch(error) {
-        console.log("Error Occured while gettinh weather", error);
-        return res.status(500).json({ msg: "Internal Sever Error" });
-    }
+    
 }
 
