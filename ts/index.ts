ae5ff9e21f7089dc280e4a0a510e46998251f69b
RichaLogiciel <richa@logiciel.io>
2024-08-02 17:40:40 +0530

    changes commit

--- ts/index.ts
+++ ts/index.ts
@@ -1,9 +1,10 @@
 import  express  from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import router from "./routes/weatherApiRoutes";
-import {connection} from "./db";
+import { connection } from "./db";
 const PORT = 8000;
 dotenv.config();
+
 const app = express();
 connection("mongodb://127.0.0.1:27017/weather_api_project");
 app.use(express.json())
